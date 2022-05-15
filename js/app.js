a66b072dca37722865ceadfa3dec57a3198b17dd
MatiMontenegro <103597788+MatiMontenegro@users.noreply.github.com>
2022-05-15 14:50:36 -0300

    Se terminan de hacer algunos ajustes de design y se sube el sitio en su formato final para el proyecto de desarrollo web de coderhouse.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+//Esto es js para las cards de los sitios creados!!
+const panels= document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+panel.addEventListener('click', () =>{
+    removeActiveClasses()
+    panel.classList.add('active')
+})
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
